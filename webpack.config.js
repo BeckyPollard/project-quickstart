a1ca65f5fa13b9f5e08a11bfda5f0c88e3ce5f6d
BeckyPollard <hello@beckypollard.com>
2020-05-21 21:25:07 -0400

    Delete dist

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'none', //for more readable main.js
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+};
