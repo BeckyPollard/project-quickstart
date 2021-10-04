44666a5776e91cf5ab676de95d94d5762cb42030
BeckyPollard <hello@beckypollard.com>
2021-10-03 21:34:39 -0400

    Restructure app

---
+++ src/scripts/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return (
+    <HelloWorld 
+      greeting='Hello World'
+    />
+  );
+};
+
+export default App;
